d27c93dcb1ffb61daa1feda089325ed913bfac37
Saswata Banerjee <78076443+saswata35@users.noreply.github.com>
2024-12-23 00:25:32 +0530

    Update init.js

--- init.js
+++ init.js
@@ -518,7 +518,7 @@ function arlo_tm_animate_text() {
   var animateSpan = jQuery(".arlo_tm_animation_text_word");
 
   animateSpan.typed({
-    strings: ["Data Science", "Software Engineer", "developer", "Analysis"],
+    strings: ["Data Scientist", "Software Engineer", "Developer", "Analyst"],
     loop: true,
     startDelay: 1e3,
     backDelay: 2e3,
